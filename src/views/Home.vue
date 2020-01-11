1ecadb1e0b0592f3c526f3077b70514629b8d0a2
Azmi Makarima <amy.azmim@gmail.com>
2020-01-11 15:12:41 +0700

    update the Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <v-row class="text-center" justify="center">
+      <v-col cols="12">
+        <v-img
+          contain
+          class="my-3"
+          height="200"
+          :src="require('../assets/logo.svg')"
+        ></v-img>
+      </v-col>
+
+      <v-col cols="12">
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to WaCharts</h1>
+        <p class="subheading font-weight-regular">
+          Analyze what's going on in your WhatsApp chat using this simple app.
+        </p>
+      </v-col>
+
+      <v-col cols="12">
+        <v-btn outlined>Start now!</v-btn>
+        <input v-show="false" type="file" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
+  name: 'Home',
 };
 </script>
