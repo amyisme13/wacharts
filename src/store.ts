a615eff40647347bd31076f57056bb70a18c682b
Azmi Makarima <amy.azmim@gmail.com>
2020-01-12 14:14:14 +0700

    add some stuff to Charts view

--- src/store.ts
+++ src/store.ts
@@ -2,15 +2,80 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import { Store } from './utils/store';
-import { analyze, MasterAccumulator, Result } from './utils/analyzer';
+import {
+  analyze,
+  Accumulator,
+  MapAccumulator,
+  MasterAccumulator,
+  Result,
+} from './utils/analyzer';
 
 Vue.use(Vuex);
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
 
 export default new Vuex.Store({
   state: {
     accumulator: null as MasterAccumulator | null,
     store: null as Store | null,
     processingTime: 0,
+  },
+
+  getters: {
+    accumulatorTop({ accumulator }) {
+      if (!accumulator) {
+        return null;
+      }
+
+      const top = {
+        user: { key: '-', value: 0 },
+        emoji: { key: '-', value: 0 },
+        day: { key: '-', value: 0 },
+      };
+
+      const userAccumulator = accumulator.byUser as MapAccumulator;
+      for (const userName in userAccumulator) {
+        const user = userAccumulator[userName];
+        if (user.message.total > top.user.value) {
+          top.user = {
+            key: userName,
+            value: user.message.total,
+          };
+        }
+      }
+
+      const allAccumulator = accumulator.all as Accumulator;
+      for (const emojiType in allAccumulator.emoji.byType) {
+        const value = allAccumulator.emoji.byType[emojiType];
+        if (value > top.emoji.value) {
+          top.emoji = {
+            key: emojiType,
+            value,
+          };
+        }
+      }
+
+      const dayAccumulator = accumulator.byDay as MapAccumulator;
+      for (const dayNum in dayAccumulator) {
+        const day = dayAccumulator[dayNum];
+        if (day.message.total > top.day.value) {
+          top.day = {
+            key: days[parseInt(dayNum)],
+            value: day.message.total,
+          };
+        }
+      }
+
+      return top;
+    },
   },
 
   mutations: {
--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -7,6 +7,11 @@
             <v-list-item-content>
               <v-list-item-title>
                 <h1 class="display-1 grey--text text--darken-3">Result</h1>
+                <span class="caption">
+                  ({{ firstMessage.date.toDateString() }}
+                  -
+                  {{ lastMessage.date.toDateString() }})
+                </span>
               </v-list-item-title>
             </v-list-item-content>
 
@@ -18,6 +23,61 @@
           </v-list-item>
 
           <v-divider></v-divider>
+
+          <v-card-text v-if="resultAvailable">
+            <v-row>
+              <v-col class="text-center" cols="3">
+                <h3 class="display-3">
+                  {{ this.store.users.length }}
+                </h3>
+                <h4 class="title font-weight-light">Users</h4>
+              </v-col>
+
+              <v-col class="text-center" cols="3">
+                <h3 class="display-3">
+                  {{ this.accumulator.all.message.total }}
+                </h3>
+                <h4 class="title font-weight-light">Messages</h4>
+              </v-col>
+
+              <v-col class="text-center" cols="3">
+                <h3 class="display-3">
+                  {{ this.accumulator.all.message.byType.media || 0 }}
+                </h3>
+                <h4 class="title font-weight-light">Pictures</h4>
+              </v-col>
+
+              <v-col class="text-center" cols="3">
+                <h3 class="display-3">
+                  {{ this.accumulator.all.emoji.total }}
+                </h3>
+                <h4 class="title font-weight-light">Emojis</h4>
+              </v-col>
+            </v-row>
+
+            <v-row class="mt-12">
+              <v-col class="text-center" cols="4">
+                <h3 class="display-2 font-weight-light">
+                  {{ this.accumulatorTop.user.key }}
+                </h3>
+                <h4 class="title font-weight-light">Top User</h4>
+              </v-col>
+
+              <v-col class="text-center" cols="4">
+                <h3 class="display-2 font-weight-light">
+                  {{ this.accumulatorTop.emoji.key }}
+                </h3>
+                <h4 class="title font-weight-light">Top Emoji</h4>
+              </v-col>
+
+              <v-col class="text-center" cols="4">
+                <h3 class="display-2 font-weight-light">
+                  {{ this.accumulatorTop.day.key }}
+                </h3>
+                <h4 class="title font-weight-light">Top Day</h4>
+              </v-col>
+            </v-row>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -26,9 +86,50 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import { MasterAccumulator } from '../utils/analyzer';
+import { Store } from '../utils/store';
 
 @Component
 export default class Charts extends Vue {
+  created() {
+    if (!this.resultAvailable) {
+      this.close();
+    }
+  }
+
+  get resultAvailable() {
+    return !!this.accumulator && !!this.store;
+  }
+
+  get accumulator() {
+    return this.$store.state.accumulator as MasterAccumulator;
+  }
+
+  get store() {
+    return this.$store.state.store as Store;
+  }
+
+  get accumulatorTop() {
+    return this.$store.getters.accumulatorTop;
+  }
+
+  get firstMessage() {
+    if (!this.store) {
+      return null;
+    }
+
+    return this.store.messages[0];
+  }
+
+  get lastMessage() {
+    if (!this.store) {
+      return null;
+    }
+
+    const length = this.store.messages.length;
+    return this.store.messages[length - 1];
+  }
+
   close() {
     this.$router.push({ name: 'home' });
   }
